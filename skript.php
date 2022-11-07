642c86726e16f102ed5c8e1c26f58ac4157a1f37
JesperStadler <jesper.stadler@icloud.com>
2022-11-07 11:01:04 +0100

    Uppgift klar

---
+++ skript.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sql";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if (
+            $row["username"] == $_POST["username"] &&
+            $row["password"] == $_POST["password"]
+        ) {
+            $login_success = true;
+            echo 'Välkommen ', $_POST['username'];
+        }
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
